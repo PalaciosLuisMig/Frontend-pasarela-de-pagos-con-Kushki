7b1f1fddeb18a3def9926c625a6fcabdac90bd47
Easy Bug <luispalacios1996@hotmail.com>
2022-03-09 01:25:38 -0500

    Updates

---
+++ precache-manifest.c31e986b2f8b1fa845047cf6fed73eba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ae7ee3ac4c15ff5db32",
+    "url": "/frontend-kushki/static/css/main.94f6ba72.chunk.css"
+  },
+  {
+    "revision": "1ae7ee3ac4c15ff5db32",
+    "url": "/frontend-kushki/static/js/main.1ae7ee3a.chunk.js"
+  },
+  {
+    "revision": "1db871dbf7f551720675",
+    "url": "/frontend-kushki/static/js/1.1db871db.chunk.js"
+  },
+  {
+    "revision": "a7884c4c0dd87dc5e4e4",
+    "url": "/frontend-kushki/static/js/runtime~main.a7884c4c.js"
+  },
+  {
+    "revision": "04c52b82a1d490253af4739670f008ad",
+    "url": "/frontend-kushki/index.html"
+  }
+];
